5f9d4db541ea556f757cdacacce60f7473dc3a80
muhammadahsanmirza <ahsanmirxa032@gmail.com>
2024-07-24 15:13:45 +0500

    Almost finished user profile page

--- src/components/StudentProfile.jsx
+++ src/components/StudentProfile.jsx
@@ -1,4 +1,3 @@
-// StudentProfile.jsx
 import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import axiosInstance from '../interceptors/axiosInstance';
@@ -18,16 +17,20 @@ function StudentProfile() {
   const [completeProfile, setCompleteProfile] = useState(false);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [userId, setUserId] = useState(false);
+  const [resumeId, setResumeId] = useState(false);
 
   useEffect(() => {
     axiosInstance.get('/student/profile')
       .then((res) => {
         console.log(res.data.data);
         setData(res.data.data);
+        setUserId(res.data.data.user_id);
         if (res.data.profile_complete) {
           setCompleteProfile(true);
           setFileName(res.data.file_name);
-
+          setResumeId(res.data.data.resume_id);
           if (res.data.file) {
             // Handle potential errors in Base64 decoding
             try {
@@ -65,21 +68,58 @@ function StudentProfile() {
   };
 
   const handleFileChange = (event) => {
+    setErrorMessage(null)
     const file = event.target.files[0];
-    setSelectedFile(file);
-  };
+    const maxFileSize = 2 * 1024 * 1024;
 
-  const handleFileUpload = () => {
-    if (selectedFile) {
-      // Here, you would typically send the file to the server
-      // using a POST request with FormData.
-      console.log('File to upload:', selectedFile);
+    if (file) {
+      if (file.type !== 'application/pdf') {
+        setErrorMessage('Invalid file format. Please upload a PDF file.');
+        setSelectedFile(null);
+        return;
+      }
 
-      // Close the dialog after file is selected
-      setIsDialogOpen(false);
+      if (file.size > maxFileSize) {
+        setErrorMessage('File size is more than the allowed 2MB.');
+        setSelectedFile(null);
+        return;
+      }
+
+      setErrorMessage(null);
+      setSelectedFile(file);
     }
   };
 
+  const handleFileUpload = () => {
+    setErrorMessage(null)
+    console.log(selectedFile)
+    if (selectedFile) {
+      // Create a FormData object
+      const formData = new FormData();
+      
+      // Append the selected file to the FormData object
+      formData.append('new_file ', selectedFile);
+      console.log(selectedFile.name)
+      const url = `/student/profile?resume_id=${resumeId}`;
+  
+      axiosInstance.put(url, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((response) => {
+        console.log('File uploaded successfully:', response.data);
+      })
+      .catch((error) => {
+        console.error('Error uploading file:', error);
+        setErrorMessage('Error uploading file:', error);
+        setSelectedFile(null);
+      });
+  
+      setIsDialogOpen(false);
+    }
+  };
+  
   return (
     <div className="w-[calc(100%-5rem)] ml-20 z-0">
       <Header breadcrumbs={breadcrumbs} />
@@ -107,10 +147,11 @@ function StudentProfile() {
           <div className='text-sm font-bold'>Major</div>
           <div className='text-gray-500 ml-12'>{data.major}</div>
         </div>
+
         {completeProfile ? (
           <div className='flex flex-row items-center p-3 m-3'>
             <div className='text-sm font-bold'>Resume</div>
-            <div className="flex flex-row rounded text-black px-2 py-1 text-sm items-center justify-center ml-24 mr-6 p-1 border gap-1">
+            <div className="flex flex-row rounded text-black py-1 text-sm items-center justify-center ml-24 mr-6 p-1 border gap-1">
               <SiAdobeacrobatreader className='w-4 h-4 text-red-800' />
               <button className="mx-1 text-sm text-gray-500" onClick={downloadFile}>
                 click to download
@@ -119,7 +160,7 @@ function StudentProfile() {
             </div>
             <div className="flex flex-row rounded bg-blue-950 text-white px-2 py-1 text-sm items-center justify-center mx-1">
               <button
-                className="select-none rounded px-6  text-white  transition-all hover:shadow-lg active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mx-3 my-1 text-xs"
+                className="select-none rounded  text-white  transition-all hover:shadow-lg active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mx-3 my-1 text-xs"
                 onClick={toggleDialog}
                 data-ripple-light="true"
                 data-dialog-target="animated-dialog"
@@ -131,9 +172,20 @@ function StudentProfile() {
         ) : (
           <div className='flex flex-row items-center p-3 m-3'>
             <div className='text-sm font-bold'>Resume</div>
-            <div className='text-gray-500 ml-12'>Resume not uploaded</div>
+            <div className="flex flex-row rounded text-black py-1 text-lg items-center justify-center ml-24 mx-8 p-1 border gap-10 h-10 px-2">
+              <SiAdobeacrobatreader className='w-4 h-4 text-red-800' />
+              <button className="mx-1 text-sm text-gray-500">
+                click to upload
+              </button>
+              <MdFileDownload className="w-4 h-4 text-green-500" />
+            </div>
           </div>
         )}
+        {errorMessage && (
+                  <div className="mt-2 text-sm text-red-500">
+                    {errorMessage}
+                  </div>
+                )}
         {isDialogOpen && (
           <div
             className="fixed inset-0 z-[999] grid h-screen w-screen place-items-center bg-black bg-opacity-60 backdrop-blur-sm"
@@ -143,29 +195,70 @@ function StudentProfile() {
               onClick={(e) => e.stopPropagation()} // Prevent click inside dialog from closing it
               className="relative m-4 w-2/5 min-w-[40%] max-w-[40%] rounded-lg bg-white font-sans text-base font-light leading-relaxed text-blue-gray-500 antialiased shadow-2xl"
             >
-              <div className="flex items-center p-4 font-sans text-2xl antialiased font-semibold leading-snug shrink-0 text-blue-gray-900">
+              <div className="flex items-center p-4 font-sans text-2xl antialiased font-semibold leading-snug shrink-0 text-blue-950">
                 Upload Resume
               </div>
-              <div className="relative p-4 font-sans text-base antialiased font-light leading-relaxed border-t border-b border-t-blue-gray-100 border-b-blue-gray-100 text-blue-gray-500">
-                <input
-                  type="file"
-                  accept=".pdf"
-                  onChange={handleFileChange}
-                  className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
-                />
+              <div className="relative p-4 font-sans text-base antialiased font-light leading-relaxed border-t border-b border-t-blue-gray-100 border-b-blue-gray-100 text-blue-gray-500 bg-white">
+                <div className="flex items-center justify-center w-full ">
+                  <label
+                    htmlFor="dropzone-file"
+                    className="flex flex-col items-center justify-center w-full h-64 border-2 border-dashed border-blue-950 rounded-lg cursor-pointer bg-gray-100 hover:bg-blue-50"
+                  >
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6 bg">
+                      <svg
+                        className="w-8 h-8 mb-4 text-blue-950"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 20 16"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                        />
+                      </svg>
+                      <p className="mb-2 text-sm text-blue-950">
+                        <span className="font-semibold">Click to upload</span> or drag and drop
+                      </p>
+                      <p className="text-xs text-gray-500">
+                        PDF files only
+                      </p>
+                    </div>
+                    <input
+                      id="dropzone-file"
+                      type="file"
+                      accept=".pdf"
+                      onChange={handleFileChange}
+                      className="hidden"
+                    />
+                  </label>
+                </div>
+                {selectedFile && (
+                  <div className="mt-2 text-sm text-blue-600">
+                    Selected file: {selectedFile.name}
+                  </div>
+                )}
+                {errorMessage && (
+                  <div className="mt-2 text-sm text-red-500">
+                    {errorMessage}
+                  </div>
+                )}
               </div>
-              <div className="flex flex-wrap items-center justify-end p-4 shrink-0 text-blue-gray-500">
+              <div className="flex flex-wrap items-center gap-4 justify-end p-4 shrink-0 text-blue-gray-500">
                 <button
                   data-ripple-dark="true"
                   onClick={toggleDialog}
-                  className="px-6 py-3 mr-1 font-sans text-xs font-bold text-red-500 uppercase transition-all rounded-lg middle none center hover:bg-red-500/10 active:bg-red-500/30"
+                  className="px-6 py-3 mr-1 font-sans text-xs font-bold text-red-500 uppercase transition-all rounded-lg middle none center hover:bg-red-500 hover:text-white active:bg-red-500/30"
                 >
                   Cancel
                 </button>
                 <button
                   data-ripple-light="true"
                   onClick={handleFileUpload}
-                  className="middle none center rounded bg-green-500 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-green-500/40 active:opacity-[0.85]"
+                  className="middle none center rounded bg-blue-950 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-green-500/20 transition-all hover:shadow-lg hover:shadow-blue-700/40 active:opacity-[0.85]"
                   disabled={!selectedFile}
                 >
                   Upload File
