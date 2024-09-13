c2766be1ab2b65677fa1e4e20a7fb73c4b8e90e2
muhammadahsanmirza <ahsanmirxa032@gmail.com>
2024-09-13 12:46:33 +0500

    Replaced repititive code of Delete Dialog

--- src/components/EditAndViewOpportunities.jsx
+++ src/components/EditAndViewOpportunities.jsx
@@ -13,6 +13,8 @@ import ViewOpportunity from "./ViewOpportunity";
 import axiosInstance from "../interceptors/axiosInstance";
 import EditOpportunity from "./EditOpportunity";
 import Loader from "./Loader";
+import DeleteDialog from "./DeleteDialog";
+
 const applicationStatus = [
   { id: 1, status: "Published", value: true },
   { id: 2, status: "Unpublished", value: false },
@@ -475,68 +477,15 @@ function EditAndViewOpportunities() {
           onClose={closeDialog}
         />
       )}
-      {confirmDialogOpen && (
-        <div>
-          <div
-            className="fixed inset-0 z-50 flex items-center  justify-center"
-            onClick={() => closeConfirmDialog()}
-          >
-            <div className="border rounded-lg shadow relative bg-white max-w-4xl z-30">
-              <div className="flex justify-end p-2">
-                <button
-                  type="button"
-                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
-                  onClick={closeConfirmDialog}
-                >
-                  <svg
-                    className="w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
-              </div>
-              <div className="p-6 pt-0 text-center">
-                <svg
-                  className="w-20 h-20 text-red-600 mx-auto"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-                <h3 className="text-xl font-normal text-gray-500 mt-5 mb-6">
-                  Do You want to delete this opportunity?
-                </h3>
-                <button
-                  className={`text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-6 py-2.5 text-center mr-2 `}
-                  onClick={() => deleteOpportunity(opportunityId)}
-                >
-                  Yes
-                </button>
-                <button
-                  onClick={closeConfirmDialog}
-                  className={`text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:ring-cyan-200 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-7 py-2.5 text-center`}
-                >
-                  No
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
+      {confirmDialogOpen && (        
+      <DeleteDialog
+        title={"Opportunity"}
+        noCallback={closeConfirmDialog}
+        yesCallback={() => deleteOpportunity(programId)}
+
+      />
+      )
+      }
       {isEditOpportunity && (
         <EditOpportunity
           {...selectedOpportunity}
